3448be1f69a00aaf79b922e345d058675b634aea
denislgx <denislg_@hotmail.com>
2020-10-28 12:15:20 -0300

    Types and Interfaces

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,87 @@
 export const GET_WEATHER = "GET_WEATHER";
 export const SET_LOADING = "SET_LOADING";
-export const SET_ERROR = "SET_ERROR"
+export const SET_ERROR = "SET_ERROR";
+export const SET_ALERT = "SET_ALERT";
+
+export interface Weather {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+}
+
+export interface WeatherData {
+    base: string;
+    clouds: {
+        all: number;
+    };
+    cod: number;
+    coord: {
+        lon: number;
+        lat: number;
+    };
+    dt: number;
+    id: number;
+    main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        humidity: number;
+    };
+    name: string;
+    sys: {
+        type: number;
+        id: number;
+        message: number;
+        country: string;
+        sunrise: number;
+        sunset: number;
+    };
+    timezone: number;
+    visibility: number;
+    weather: Weather[];
+    wind: {
+        speed: number;
+        deg: number;
+    };
+}
+
+export interface WeatherError {
+    cod: string;
+    message: string;
+}
+
+export interface WeatherState {
+    data: WeatherData | null;
+    loading: boolean;
+    error: string;
+}
+
+interface GetWeatherAction {
+    type: typeof GET_WEATHER;
+    payload: WeatherData;
+}
+
+interface SetLoadingAction {
+    type: typeof SET_LOADING;
+}
+
+interface SetErrorAction {
+    type: typeof SET_ERROR;
+    payload: string;
+}
+
+export type WeatherAction =
+    | GetWeatherAction
+    | SetLoadingAction
+    | SetErrorAction;
+
+export interface AlertAction {
+    type: typeof SET_ALERT;
+}
+
+export interface AlertState {
+    message: string;
+}
